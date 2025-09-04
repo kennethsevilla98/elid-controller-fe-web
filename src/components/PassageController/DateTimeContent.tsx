f5d850b248be78b165b5354e1e008560a3e0233a
nuc poc prod <work@nuc.com>
2025-09-04 11:27:44 +0800

    fix: header layout

--- src/components/PassageController/DateTimeContent.tsx
+++ src/components/PassageController/DateTimeContent.tsx
@@ -9,20 +9,12 @@ const DateTimeContent = () => {
 
   return (
     <div className="flex gap-8 h-[70px] justify-between px-8 mt-8 mb-[-30px]">
-      <div className=" flex justify-center items-center gap-2 w-full text-white bg-[#003F98] text-3xl font-bold px-8 rounded-lg">
+      <div className=" flex justify-between items-center gap-2 w-full text-white bg-[#003F98] px-8 text-4xl font-bold  rounded-lg">
         {/* <CalendarDays /> */}
         <p>{todate}</p>
+             <p>{philippinesTime}</p>
       </div>
-      <div className="flex items-center gap-8 justify-center text-white bg-[#003F98] text-3xl font-bold  rounded-lg px-8 w-full">
-        {/* <img src={PH_FLAG} alt="phil" /> */}
-        <p>{philippinesTime}</p>
-      </div>
-      {/* <div className="flex gap-4"> */}
-      {/* <div className="flex items-center  justify-center gap-8  text-white bg-[#003F98] text-3xl font-bold  rounded-lg px-8  w-96">
-          <img src={JP_JFLAG} alt="japan" />
-          <p>{japanTime}</p>
-        </div> */}
-      {/* </div> */}
+  
     </div>
   );
 };
